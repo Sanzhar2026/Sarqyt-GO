ec50b83ccb09de455d417bd8f3bf509a269306d3
Жумабаев Ильяс <iliaszhymabaev@MacBook-Air-Zumabaev.local>
2026-06-30 08:35:25 +0500

    Initial commit - adding my project files

--- app/components/OfferCard.tsx
+++ app/components/OfferCard.tsx
@@ -492,15 +492,14 @@ export default function OfferCard({
           />
         </button>
         
-     <button 
+<button 
   onClick={handleIconClick}
-  className="p-1.5 bg-gray-100/80 hover:bg-gray-200/80 rounded-lg transition"
+  className="p-1.5"
 >
-  <svg className="w-4 h-4 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+  <svg className="w-4 h-4 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
   </svg>
 </button>
-
 
       </div>
       
